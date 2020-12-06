62e4af95bcf26ef9d3e7950f71d8c270b27643ec
Jay_WooKJ <natcho9010@gmail.com>
2020-12-06 11:30:44 +0900

    clock done

---
+++ index.js
@@ -0,0 +1,13 @@
+const clockDiv = document.querySelector("#clockDiv");
+
+//시계를 html에 추가
+function addClock(){
+  let now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+  const second = now.getSeconds();
+
+  clockDiv.innerHTML = `<h1>${hour}:${minute}:${second}</h1>`;
+}
+
+setInterval(addClock, 1000);
