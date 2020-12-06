c7d96c9a79d749c278707cc9edc05eb8257a2f2b
Jay_WooKJ <natcho9010@gmail.com>
2020-12-06 18:19:51 +0900

    random image done

---
+++ randomBackGround.js
@@ -0,0 +1,18 @@
+const imageUrls = [
+    "https://my.alfred.edu/zoom/_images/foster-lake.jpg",
+    "http://s1.picswalls.com/thumbs2/2016/03/29/beautiful-nature-background_042455537_304.jpg",
+    "http://s1.picswalls.com/thumbs2/2016/03/29/beautiful-beautiful-nature-wallpaper_042456813_304.jpg",
+    "https://wallpapercave.com/wp/zldNYKr.jpg",
+    "https://wallpapercave.com/wp/MkslXhR.jpg",
+    "https://wallpapercave.com/wp/drHGKXe.jpg",
+    "https://wallpapercave.com/wp/aLrx4ut.jpg"
+]
+
+const html = document.querySelector("html")
+
+const randomIndex = Math.ceil(Math.random() * imageUrls.length) - 1;
+
+console.log(randomIndex)
+
+html.style.backgroundImage = `url(${imageUrls[randomIndex]})`;
+html.style.backgroundSize = "cover";
